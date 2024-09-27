8f5e0aba52288a0ea1037313fcaf2af6f191f6fe
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-09-27 22:42:45 +0530

    Skills were added for the About me Section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 //import './App.css';
 import Navbar from "./components/nav bar/nav bar";
 import Intro from "./components/Intro/intro";
+import Skills from "./components/Skills/skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <Skills/>
     </div>
   );
 }
