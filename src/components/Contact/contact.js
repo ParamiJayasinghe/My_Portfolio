14c1c6ee6df5a022a7abdc039de76b54fbb35b4b
ParamiJayasinghe <155864571+ParamiJayasinghe@users.noreply.github.com>
2024-10-03 23:01:09 +0530

    Add files via upload

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Contact = () => {
+    return(
+        <section className="contactPage">
+            <div id="myClients">
+
+            </div>
+
+            <div id="cintactMe">
+
+
+            </div>
+
+
+        </section>
+    
+    ) 
+}
+
+export default Contact
