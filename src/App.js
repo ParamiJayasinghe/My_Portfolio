286f0f9b51f8e90b9641b870fc32c1f6c9396d08
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-10-19 20:29:18 +0530

    save local changes before merge

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills/>
       <Works/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -4,6 +4,10 @@ import facebook from '..//..//images/facebook.png';
 import kfc from '..//..//images/kfc.png';
 import uber from '..//..//images/uber.jpg';
 import coolPlanet from '..//..//images/coolplanet.png';
+import FbIcon from "..//..//images/FBIcon.jpeg";
+import YtICon from "..//..//images/ytIconf.png";
+import TwitterIcon from "..//..//images/twitterIcon.jpeg";
+import InstaIcon from "..//..//images/instaIcon.jpeg";
 
 
 const Contact = () => {
@@ -23,6 +27,20 @@ const Contact = () => {
             </div>
 
             <div id="contactMe">
+                <h1 className="contactPageTitle">Contact Me</h1>
+                <span className="contactDEsc">PLease fill the form below to discuss the works.</span>
+                <form className="contactForm">
+                    <input type="text" className="name" placeholder="Your Name" />
+                    <input type="text" className="email" placeholder="Your Email" />
+                    <textarea className ="msg" name="message" rows={5} placeholder="Your Message" ></textarea>
+                    <button type="submit" value='send' className="submitBtn">Submit</button>
+                    <div className="links">
+                        <img src={FbIcon} alt="facebook" className="link" />
+                        <img src={YtICon} alt="youtube" className="link" />
+                        <img src={InstaIcon} alt="insta" className="link" />
+                        <img src={TwitterIcon} alt="twitter" className="link" />
+                    </div>
+                </form>
 
 
             </div>
