4fcd54ce5f13efe2d10e2d30d8216cfbb39c3ac4
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-11-03 19:06:17 +0530

    footer recreate

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-import './footer.css';
+import './footer.css'
 
 const Footer = () => {
     return(
         <footer className="footer">
-            Copyright &#169; 2024 Parami Jayasinghe.All rightd recieved.
+        Copyright &#169;2023 Smith Williams.All right received.
         </footer>
     ) 
 }
 
-=======
-import React from "react";
-import './footer.css';
-
-const Footer = () => {
-    return(
-        <footer className="footer">
-            Copyright &#169; 2024 Parami Jayasinghe.All rightd recieved.
-        </footer>
-    ) 
-}
-
->>>>>>> c2acc960b818c5c2e846eb3c0892fd92286a440d
 export default Footer
