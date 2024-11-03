806f20b291d9afcb7bfa73b5168e542a5fa3a98c
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-11-03 21:27:43 +0530

    works.js corrected

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import './works.css';
 import Portfolio1 from '../../images/healthcare system.webp';
@@ -29,34 +28,4 @@ const Works=() =>{
        </section>
     )
 }
-=======
-import React from "react";
-import './works.css';
-import Portfolio1 from '../../images/healthcare system.webp';
-import Portfolio2 from '../../images/hotel booking system.jpg';
-import Portfolio3 from '../../images/movie review.jpg';
-import Portfolio4 from '../../images/new recipe.png';
-import Portfolio5 from '../../images/online shopping.jpg';
-import Portfolio6 from '../../images/song recomendation system.webp';
-
-
-const Works=() =>{
-
-    return(
-       <section id="works">
-        <h2 className="worksTitle">My Portfolio</h2>
-        <span className="worksDescription">A descriotion sentence</span>
-        <div className="worksImgs">
-            <img src={Portfolio1} alt="portfolio1" className="worksImg" />
-            <img src={Portfolio2} alt="portfolio2" className="worksImg" />
-            <img src={Portfolio3} alt="portfolio3" className="worksImg" />
-            <img src={Portfolio4} alt="portfolio4" className="worksImg" />
-            <img src={Portfolio5} alt="portfolio5" className="worksImg" />
-            <img src={Portfolio6} alt="portfolio6" className="worksImg" />
-
-        </div>
-       </section>
-    )
-}
->>>>>>> c2acc960b818c5c2e846eb3c0892fd92286a440d
 export default Works
